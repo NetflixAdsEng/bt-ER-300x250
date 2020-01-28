9e196620c1166fdaa82377a0d1916e357c2b1a07
Derek Miranda <derek.miranda@ff0000.com>
2020-01-28 11:43:39 -0800

    Bumps up CTA for Indonesian

--- src/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ src/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1580166737770,
+  "updated": 1580240615759,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
