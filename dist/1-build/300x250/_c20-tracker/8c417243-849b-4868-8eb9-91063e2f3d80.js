88b2001dbe0e5d3d03e5b25c18cf9e369061c414
Derek Miranda <derek.miranda@ff0000.com>
2020-02-03 13:19:24 -0800

    Hebrew: CTA text size increase

--- dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1580328624417,
+  "updated": 1580764724356,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
