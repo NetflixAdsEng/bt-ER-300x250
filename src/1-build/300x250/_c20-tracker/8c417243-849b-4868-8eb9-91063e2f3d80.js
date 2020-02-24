4677c3145a6ab05f6102f1c8c623c688afe04e16
Derek Miranda <derek.miranda@ff0000.com>
2020-02-24 14:15:33 -0800

    Bigger vert space for Arabic CTA

--- src/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ src/1-build/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1580764724356,
+  "updated": 1582582530667,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
