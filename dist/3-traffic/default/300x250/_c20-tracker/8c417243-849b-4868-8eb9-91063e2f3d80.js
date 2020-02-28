f6e6e94a42cb4eda0af8de98363724bee2780a8e
Derek Miranda <derek.miranda@ff0000.com>
2020-02-27 17:52:31 -0800

    Adds Upper-Right Ratings Toggle
    - 300x600 + 970x250 CTA fixes

--- dist/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
+++ dist/3-traffic/default/300x250/_c20-tracker/8c417243-849b-4868-8eb9-91063e2f3d80.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1582679727578,
+  "updated": 1582854722797,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x250",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x250",
